4aeaa70390b7f623d1585fd76b48e140bf422651
Chase McCarty <chase_mccarty@Chases-MacBook-Pro.local>
2012-04-01 22:48:48 -0600

    Initial Commit

---
+++ RageFaces2.0/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RageFaces2.0
+//
+//  Created by Chase McCarty on 12-04-01.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RageFaces2.0/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RageFaces2.0
+//
+//  Created by Chase McCarty on 12-04-01.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
